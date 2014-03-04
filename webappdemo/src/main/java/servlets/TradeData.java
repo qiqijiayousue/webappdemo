09fa03df328e747eced080ea97296e7391462dea
qiqijiayousue <qiqijiayousue@163.com>
2014-03-04 16:06:36 +0800

    commit tradedata.java

---
+++ webappdemo/src/main/java/servlets/TradeData.java
@@ -0,0 +1,26 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by miaoxi.zj on 14-2-18.
+ */
+public class TradeData extends HttpServlet {
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+
+    }
+}
+
